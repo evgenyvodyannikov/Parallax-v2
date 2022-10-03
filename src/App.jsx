09795e47545ae14df24e75517c7c5a10626f6d2a
eugenevodyannikov <vodyannikov.eugene@gmail.com>
2022-10-03 11:38:37 +0500

    structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,72 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import layerBase from './assets/image/layer-base.png'
+import layerFront from './assets/image/layer-front.png'
+import layerMiddle from './assets/image/layer-middle.png'
+import dungeon from './assets/image/dungeon.jpg'
+import ground from './assets/image/ground.png'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Parallax pages={2}>
+        <ParallaxLayer
+        offset={0}
+        speed={0.3}
+        factor={1}
+        style={{backgroundImage: `url(${layerBase})`,
+        backgroundSize: 'cover'}}>
+        </ParallaxLayer>
+
+
+        <ParallaxLayer
+        offset={0}
+        speed={0}
+        factor={1}
+        style={{backgroundImage: `url(${layerMiddle})`,
+        backgroundSize: 'cover'}}>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+        offset={0}
+        speed={0}
+        factor={1}
+        style={{backgroundImage: `url(${layerFront})`,
+        backgroundSize: 'cover'}}>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+        offset={0.5}
+        speed={0}
+        factor={1}
+        style={{backgroundImage: `url(${ground})`,
+        backgroundSize: 'cover'}}>
+        </ParallaxLayer>
+
+
+        <ParallaxLayer
+        offset={1}
+        speed={0}
+        factor={1}
+        style={{backgroundImage: `url(${dungeon})`,
+        backgroundSize: 'cover'}}>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0.4} speed={2}>
+          <h1>Hello</h1>
+        </ParallaxLayer>
+
+
+        <ParallaxLayer offset={1.5} speed={4}>
+          <h1>Next page</h1>
+        </ParallaxLayer>
+
+      </Parallax>
+
     </div>
   )
 }
